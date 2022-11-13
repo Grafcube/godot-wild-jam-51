c108f38b6fe06fdcd3a943984d28269dc7a8ad02
Abhinav Chennubhotla <51199011+PhoenixFlame101@users.noreply.github.com>
2022-11-13 14:19:38 +0530

    Add bullet spawner

---
+++ src/gun/bullet.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+export(int, 0, 12000) var speed: = 1
+var direction = Vector2.ZERO
+
+func _ready():
+	pass
+
+func _process(delta):
+	position += speed * delta * direction.normalized()
+
+
+func _on_screen_exited():
+	queue_free()
