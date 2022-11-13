cb1837315ba70f5ed044e0764264cf479c5d7788
Abhinav Chennubhotla <51199011+PhoenixFlame101@users.noreply.github.com>
2022-11-13 14:19:38 +0530

    Add bullet spawner

--- src/player/player.gd
+++ src/player/player.gd
@@ -4,20 +4,26 @@ export(int, 0, 12000) var speed: = 1
 
 func _ready():
 	pass
-	
+
 func _physics_process(delta):
 	var direction: = Vector2.ZERO
-	
+
 	var input_strengths: Dictionary = {
 	  "left": Input.get_action_strength("player_left"),
 	  "right": Input.get_action_strength("player_right"),
 	  "up": Input.get_action_strength("player_up"),
 	  "down": Input.get_action_strength("player_down")
 	}
-	
+
 	direction = Vector2(input_strengths.right - input_strengths.left,
 				input_strengths.down - input_strengths.up)
 	direction = direction.clamped(1.0)
 
 	# warning-ignore:return_value_discarded
 	move_and_slide(direction * speed * delta, Vector2.ZERO)
+
+
+func _on_timeout():
+	var spawned_bullet = $Bullet.create_instance()
+	spawned_bullet.global_position = self.global_position
+	spawned_bullet.direction = get_local_mouse_position()
