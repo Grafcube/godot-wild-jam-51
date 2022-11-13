4c228e6caddc383deae70cc5fd72b1afa2bfd214
Grafcube <grafcube@disroot.org>
2022-11-13 14:35:23 +0530

    Run formatter

--- src/gun/bullet.gd
+++ src/gun/bullet.gd
@@ -1,10 +1,12 @@
 extends Area2D
 
-export(int, 0, 12000) var speed: = 1
+export(int, 0, 12000) var speed := 1
 var direction = Vector2.ZERO
+
 
 func _ready():
 	pass
+
 
 func _process(delta):
 	position += speed * delta * direction.normalized()
